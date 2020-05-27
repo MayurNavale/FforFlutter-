f4795eec40ac2333710bd8f58f4501108f822e9a
MayurNavale <45039452+MayurNavale@users.noreply.github.com>
2020-05-27 14:15:26 +0530

    another code

---
+++ flutterCode/12_statefull example.dart
@@ -0,0 +1,64 @@
+
+  
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+
+
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+    home :Scaffold(
+      appBar:AppBar(
+        //:Colors.green,
+      title:Text('complex example'),
+      ),
+      body:MyWidget(),
+     ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget{
+  
+  _MyWidgetState  createState()=>_MyWidgetState();
+  
+}
+class _MyWidgetState extends State<MyWidget>{
+String textString='hello world';
+Widget build(BuildContext context){
+  return  Column(
+    //alignment:Alignment.center,
+  children:[
+     Text(textString),
+    
+         RaisedButton(
+           child: const Text('hello'),
+             color:Colors.red,
+                 onPressed:(){
+                   dosomething();
+          
+        },
+           
+      ),
+    Padding(
+      padding:EdgeInsets.all(20),
+      child:TextField(
+     decoration:InputDecoration(
+       border: InputBorder.none,
+       hintText: 'write here'
+     ),
+  ),
+    ),
+    ],
+);
+}
+  void dosomething(){
+    setState((){
+      textString='hello flutter';
+    });
+  }
+}
+
